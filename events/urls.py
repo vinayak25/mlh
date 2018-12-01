4dd09d014e37663b7c931d9ed0db8ad09f0c1178
Divyansh Kamboj <agilephoenix815.dk@gmail.com>
2018-12-01 15:42:08 +0530

    ADD new-hack/

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from events import views
 
 urlpatterns = [
-    path('new/', views.create_tech_event),
+    path('new-tech/', views.create_tech_event),
+    path('new-hack/', views.create_hack_event)
 ]
--- events/views.py
+++ events/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from events.forms import EventForm
 from events.models import Event
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='/custom_auth/login')
 def create_tech_event(request):
     if request.method == 'POST':
         form = EventForm(request.POST)
@@ -11,11 +14,33 @@ def create_tech_event(request):
             event_type = 1
             user_id = request.user.id
             date = form.cleaned_data['date']
-            event = Event(user = request.user, name = name, description = description, type = event_type, date = date)
+            event = Event(user=request.user, name=name,
+                          description=description, type=event_type, date=date)
             event.save()
             return redirect('http://127.0.0.1:8000/custom_auth/profile/')
         else:
-            return render(request, 'new_event.html', { 'form': form})
+            return render(request, 'new_event.html', {'form': form})
+    else:
+        form = EventForm()
+        return render(request, "new_event.html", {'form': form})
+
+
+@login_required(login_url='/custom_auth/login')
+def create_hack_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            event_type = 2
+            user_id = request.user.id
+            date = form.cleaned_data['date']
+            event = Event(user=request.user, name=name,
+                          description=description, type=event_type, date=date)
+            event.save()
+            return redirect('http://127.0.0.1:8000/custom_auth/profile/')
+        else:
+            return render(request, 'new_event.html', {'form': form})
     else:
         form = EventForm()
         return render(request, "new_event.html", {'form': form})
