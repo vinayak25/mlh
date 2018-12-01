71c73ee6fd0bb965bf9b676030352650cea43275
Vinayak Sarawagi <vinayaksarawagi@pop-os.localdomain>
2018-12-01 12:38:46 +0530

    ADD Role User Relation

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Role(models.Model):
     name = models.CharField(max_length=255)
     permission = models.ManyToManyField("Permission")
+    user = models.ManyToManyField(User)
     def __str__(self):
         return self.name
 
 
 class Permission(models.Model):
     name = models.CharField(max_length=255)
-
 
