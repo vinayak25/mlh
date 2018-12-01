4349cbfa22a30939aaedee52224c62e865016035
Divyansh Kamboj <agilephoenix815.dk@gmail.com>
2018-12-01 16:13:12 +0530

    ADD attendee model

--- events/models.py
+++ events/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
-    type = models.IntegerField(null = True)
+    type = models.IntegerField(null=True)
     date = models.DateField("Date", default=datetime.date.today)
     description = models.CharField(max_length=512)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,3 +19,11 @@ class Event(models.Model):
             return 'Tech Event'
         else:
             return 'Hackathon'
+
+
+class Attendees(models.Model):
+    event = models.ManyToManyField("Event")
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.event.name
