13fb59f3540cfde3a37c2c41181201dea4871d97
Vinayak Sarawagi <vinayaksarawagi@pop-os.localdomain>
2018-12-01 12:30:27 +0530

    ADD Permission ROle

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -3,13 +3,12 @@ from django.db import models
 # Create your models here.
 class Role(models.Model):
     name = models.CharField(max_length=255)
-    
+    permission = models.ManyToManyField("Permission")
     def __str__(self):
         return self.name
 
 
-class PermissionRole(models.Model):
-    role = models.ManyToManyField("Role")
-    def __str__(self):
-        return self.name
+class Permission(models.Model):
+    name = models.CharField(max_length=255)
+
 
