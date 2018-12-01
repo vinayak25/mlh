12d611a67d58ebddc73a21b6961373a3004c9ced
Vinayak Sarawagi <vinayaksarawagi@pop-os.localdomain>
2018-12-01 17:44:56 +0530

    MODIFY templates

--- events/urls.py
+++ events/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('new-tech/', views.create_tech_event, name='new_tech_event'),
     path('new-hack/', views.create_hack_event, name='new_hack_event'),
     path('<int:event_id>/attend-event/', views.mark_event_attendee, name='attend-event'),
+    path('tech-events/', views.get_tech_events, name='get_tech_events'),
+    path('hackathons/', views.get_hackathons, name='get_hackathons'),
 ]
--- events/views.py
+++ events/views.py
@@ -6,44 +6,51 @@ from django.shortcuts import get_object_or_404
 
 @login_required(login_url='/custom_auth/login')
 def create_tech_event(request):
-    if request.method == 'POST':
-        form = EventForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            description = form.cleaned_data['description']
-            event_type = 1
-            user_id = request.user.id
-            date = form.cleaned_data['date']
-            event = Event(user=request.user, name=name,
-                          description=description, type=event_type, date=date)
-            event.save()
-            return redirect('http://127.0.0.1:8000/custom_auth/profile/')
+    user = request.user
+    if user.role_set.filter(id = 1):
+        if request.method == 'POST':
+            form = EventForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                description = form.cleaned_data['description']
+                event_type = 1
+                user_id = request.user.id
+                date = form.cleaned_data['date']
+                event = Event(user=request.user, name=name,
+                            description=description, type=event_type, date=date)
+                event.save()
+                return redirect('http://127.0.0.1:8000/custom_auth/profile/')
+            else:
+                return render(request, 'new_event.html', {'form': form})
         else:
-            return render(request, 'new_event.html', {'form': form})
+            form = EventForm()
+            return render(request, "new_event.html", {'form': form})
     else:
-        form = EventForm()
-        return render(request, "new_event.html", {'form': form})
-
+        return render(request, 'not_permitted.html')
 
 @login_required(login_url='/custom_auth/login')
 def create_hack_event(request):
-    if request.method == 'POST':
-        form = EventForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            description = form.cleaned_data['description']
-            event_type = 2
-            user_id = request.user.id
-            date = form.cleaned_data['date']
-            event = Event(user=request.user, name=name,
-                          description=description, type=event_type, date=date)
-            event.save()
-            return redirect('http://127.0.0.1:8000/custom_auth/profile/')
+    user = request.user
+    if user.role_set.filter(id = 1):
+        if request.method == 'POST':
+            form = EventForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                description = form.cleaned_data['description']
+                event_type = 2
+                user_id = request.user.id
+                date = form.cleaned_data['date']
+                event = Event(user=request.user, name=name,
+                            description=description, type=event_type, date=date)
+                event.save()
+                return redirect('http://127.0.0.1:8000/custom_auth/profile/')
+            else:
+                return render(request, 'new_event.html', {'form': form})
         else:
-            return render(request, 'new_event.html', {'form': form})
+            form = EventForm()
+            return render(request, "new_event.html", {'form': form})
     else:
-        form = EventForm()
-        return render(request, "new_event.html", {'form': form})
+        return render(request, 'not_permitted.html')
 
 @login_required(login_url='/custom_auth/login')
 def mark_event_attendee(request, event_id):
@@ -52,3 +59,13 @@ def mark_event_attendee(request, event_id):
     
     event.attendee.add(user)
     return redirect('http://127.0.0.1:8000/custom_auth/profile/')
+
+@login_required(login_url='/custom_auth/login')
+def get_tech_events(request):
+    events = Event.objects.filter(type=1)
+    return render(request, 'tech_events.html', { 'user': request.user, 'events': events })
+
+@login_required(login_url='/custom_auth/login')
+def get_hackathons(request):
+    events = Event.objects.filter(type=2)
+    return render(request, 'profile.html', { 'user': request.user, 'events': events })
